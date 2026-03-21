23a5e4db96bd6cbd6046a84117e5bd81eab9d902
John Njenga <johntezjohn502@gmail.com>
2026-03-21 17:18:40 +0300

    Admin controller

--- src/com/university/system/controller/AdminController.java
+++ src/com/university/system/controller/AdminController.java
@@ -3,7 +3,5 @@ package com.university.sysyem.controller;
 public class AdminController {
   public void addCourse(String course) {
     Connection conn = DatabaseConnection.getConnection();
-
-    String sql = "insert into Student "
   }
 }
